a0e643ec722d0598bfe432c06bd1eb9f1215cead
nadkon <nadkon@gmail.com>
2023-05-18 15:56:17 +0300

    project: instalation of semantic react UI and moment

--- project/src/index.js
+++ project/src/index.js
@@ -2,35 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "semantic-ui-css/semantic.min.css";
 
-//Form
-const form = ReactDOM.createRoot(document.getElementById('form'));
+
+const form = ReactDOM.createRoot(document.getElementById('App'));
 form.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-// Display weather information
-const info = ReactDOM.createRoot(
-  document.getElementById("weather-info")
-);
-info.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// Display forecast
-const forecast = ReactDOM.createRoot(
-  document.getElementById("weather-forecast")
-);
-forecast.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
-
